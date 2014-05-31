35695998e7a39e8c0a6423babf04ed448a805053
Deepak Bansal <deepak.bansal@snaplion.com>
2014-05-31 19:05:43 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- js/new_script.js
+++ js/new_script.js
@@ -757,6 +757,19 @@ function getPageList(msg) {
 				}
 			}
 		} else {
+			if(uninstalled.length) {	
+				installedAppPages.append('<h4 class="install-heading">Install Facebook Wizard for on More Pages?</h4>');
+
+				for(var k = 0; k < uninstalled.length; k++) {
+					// uninstalledPage.find("div.unistalledPageName span.toggleradio").attr("data-id",uninstalled[k].id).attr("data-name",uninstalled[k].name);
+					uninstalledPage.find("img.uninstalledAppImage").attr("src","https://graph.facebook.com/"+uninstalled[k].id+"/picture?height=64&width=64");
+					uninstalledPage.find(".pageName").text(uninstalled[k].name);
+					uninstalledPage.find(".pageCategory").text(uninstalled[k].category);
+					var innerHtml = uninstalledPage.html();
+					uninstalledAppPages.append(innerHtml);
+				}
+			}
+
 			for(var i = 0; i < installed.length; i++) {
 				installedPage.find("div.installedAppPage,div.appLinkDiv").attr("data-id",installed[i].id);
 				installedPage.find("img.installedAppImage").attr("src","https://graph.facebook.com/"+installed[i].id+"/picture?height=64&width=64");
@@ -766,18 +779,6 @@ function getPageList(msg) {
 				installedAppPages.append(innerHtml);
 			}
 
-			if(uninstalled.length) {	
-				installedAppPages.append('<h4 class="install-heading">Install Facebook Wizard for on More Pages?</h4>');
-
-				for(var k = 0; k < uninstalled.length; k++) {
-					// uninstalledPage.find("div.unistalledPageName span.toggleradio").attr("data-id",uninstalled[k].id).attr("data-name",uninstalled[k].name);
-					uninstalledPage.find("img.uninstalledAppImage").attr("src","https://graph.facebook.com/"+uninstalled[k].id+"/picture?height=64&width=64");
-					uninstalledPage.find("span.pageName").text(uninstalled[k].name);
-					uninstalledPage.find("span.pageCategory").text(uninstalled[k].category);
-					var innerHtml = uninstalledPage.html();
-					uninstalledAppPages.append(innerHtml);
-				}
-			}
 			$('.installedApps').show();
 		}
 
