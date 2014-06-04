e757fd871821307e06840191427b1f9050a822d9
Deepak Bansal <deepak.bansal@snaplion.com>
2014-06-04 21:53:29 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- import_functions.php
+++ import_functions.php
@@ -923,12 +923,13 @@ $post_count = 0;
 
 		$context  = stream_context_create($options);
 		$result = file_get_contents($url, false, $context);
+		print_r($result);
 		$result_data = json_decode($result,true);
 
 		//status tells if the data has been posted or not
 		// print_r($item_count);
 		$item_count['output'] = $result_data['result']['status'];
-		print_r(json_encode($result));
+		// print_r(json_encode($item_count));
 		// print_r($result_data['result']['status']);
 		// if( !$result_data['result']['status'] ) {	
 		// 	//if data has not been posted then stop the further execution
