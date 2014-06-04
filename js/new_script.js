fe7e3c72b7b1a229870accef925b331a9dd02b46
Deepak Bansal <deepak.bansal@snaplion.com>
2014-06-04 12:29:03 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- js/new_script.js
+++ js/new_script.js
@@ -668,7 +668,7 @@ function getPageList(msg) {
 				}
 				uninstalledAppPages.find("h4.newAppMessage").text(message);
 
-				var uninstalledNewPageHtml = uninstalledNewPage.find('.fb_left-cma').clone();
+				var uninstalledNewPageHtml = uninstalledNewPage.clone();
 				for(var k = 0; k < uninstalled.length; k++) {
 					uninstalledNewPageHtml.find("img.uninstalledAppImage").attr("src","https://graph.facebook.com/"+uninstalled[k].id+"/picture?height=64&width=64");
 					uninstalledNewPageHtml.find(".pageName").text(uninstalled[k].name);
@@ -678,7 +678,6 @@ function getPageList(msg) {
 					uninstalledNewPageHtml.find(".newAppRadio").attr("id", 'radio' + k);
 					uninstalledNewPageHtml.find(".newAppRadio").next('label').attr("for", 'radio' + k);
 
-					console.log(uninstalledNewPageHtml.html());
 					uninstalledNewAppPages.append(uninstalledNewPageHtml.html());
 				}
 			}
