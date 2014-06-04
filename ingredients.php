5f9c79dca36b4ae0b0ca677f0b58fb10bb4735e0
Deepak Bansal <deepak.bansal@snaplion.com>
2014-06-04 20:15:39 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- ingredients.php
+++ ingredients.php
@@ -29,10 +29,11 @@
 	</head>
 
 	<body>
-		<div class="loader-bg-main" id="loadingCircle" style="position: relative;">
-			<div class="loader-bg"><img src="img/loader.GIF" width="40"></div>
-		</div>
-		<div id="ingredientsPage" class="container-fluid main_content">
+		<div id="ingredientsPage" class="container-fluid main_content" style="position: relative;">
+			<div class="loader-bg-main" id="loadingCircle">
+				<div class="loader-bg"><img src="img/loader.GIF" width="40"></div>
+			</div>
+		
 			<div id="crop_modal" class="modal fade hide" tabindex="-1" role="dialog" aria-labelledby="crop_modal_header" aria-hidden="true">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
