effd16981721fa50438e07e8a19e7bd0b2b1543d
Deepak Bansal <deepak.bansal@snaplion.com>
2014-06-09 17:10:46 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- imported.php
+++ imported.php
@@ -276,7 +276,7 @@
 
 							<script>
 							  	var handler = StripeCheckout.configure({
-								    key: 'pk_test_idc5V67kywOPFOub6f733v6j',
+								    key: 'pk_test_8Bz7OsUYFmfnDABGPQrEdcdS',
 								    image: 'img/snaplion_round_logo.jpg',
 								    token: function(token, args) {
 								    	console.log(token);
