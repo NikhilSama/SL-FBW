437cdb2b457e86d6eb8ac8e0c7d7c84b403a87b1
Deepak Bansal <deepak.bansal@snaplion.com>
2014-06-10 13:00:16 +0530

    Minor Iframe Width
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- imported.php
+++ imported.php
@@ -77,7 +77,7 @@
 		</div>
 		<div class="fb_maincontainer">
 			<iframe src="<?php echo 'https://fbwsimulator.snaplion.com/#/?app_id='.$mobapp_id; ?>" 
-				frameborder="0" style="height: 390px; width: 220px;margin-top: 65px;padding-right: 27px;float: right;">
+				frameborder="0" style="height: 390px; width: 220px;margin-top: 65px;padding-right: 29px;float: right;">
 			</iframe>
 
 			<div class="fb_left">
