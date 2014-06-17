5a115c2b505acdee15369efd473321a251834133
Deepak Bansal <deepak.bansal@snaplion.com>
2014-06-17 17:25:59 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- js/new_script.js
+++ js/new_script.js
@@ -220,8 +220,8 @@ function checkLogin(msg) {
 $(document).on("click", "a.appLinkDiv",function(){
 	//getting the page id of the div clicked upon
 	var pageId = $(this).data("id");
-	window.open( "https://www.facebook.com/"+pageId+"?id="+pageId+"&sk=app_"+INSTALLED_APP_ID,"_blank");
-	// window.top.location.href = "https://www.facebook.com/"+pageId+"?id="+pageId+"&sk=app_"+INSTALLED_APP_ID,"_blank";
+	// window.open( "https://www.facebook.com/"+pageId+"?id="+pageId+"&sk=app_"+INSTALLED_APP_ID,"_blank");
+	window.top.location.href = "https://www.facebook.com/"+pageId+"?id="+pageId+"&sk=app_"+INSTALLED_APP_ID,"_blank";
 });
 
 $("body").on("click", "span.toggleradio",function(){
