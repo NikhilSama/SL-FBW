554a9c3777959e218d98cc316d5cc9ca31e02d25
Deepak Bansal <deepak.bansal@snaplion.com>
2014-07-11 19:08:42 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- index.php
+++ index.php
@@ -125,35 +125,17 @@
 				print_r($_SESSION);
 				print_r($_REQUEST);
 
+				echo '<script type="text/javascript">alert(top.location.href);</script>';
 
-				function parse_signed_request($signed_request) {
-				  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
-
-				  $secret = APPSECRET; // Use your app secret here
-
-				  // decode the data
-				  $sig = base64_url_decode($encoded_sig);
-				  $data = json_decode(base64_url_decode($payload), true);
-
-				  // confirm the signature
-				  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
-				  if ($sig !== $expected_sig) {
-				    error_log('Bad Signed JSON signature!');
-				    return null;
-				  }
-
-				  return $data;
-				}
-
-				function base64_url_decode($input) {
-				  return base64_decode(strtr($input, '-_', '+/'));
-				}
-
-				$signedReqData = parse_signed_request($_REQUEST['signed_request']);
-				print_r($signedReqData);
 				exit;
 
 				$fbid = $fbObject->getFBID();
+
+
+
+
+
+
 
 				if(is_numeric($fbid)) {
 					$db->execute_query("select * from ".USERS." where fbid = ".$fbid);
