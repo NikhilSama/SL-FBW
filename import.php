c16f22f8cd5949a4a680adfc176c1f5cdbdf8346
Deepak Bansal <deepak.bansal@snaplion.com>
2014-07-31 16:37:22 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- import.php
+++ import.php
@@ -58,23 +58,24 @@
 	$posts = $fbObject->api($page_id."/feed?fields=picture,place,message,object_id,source,created_time,type&limit=5000");
 	if(!empty($posts['data'])) {
 		$postCount += count($posts['data']);
+		feedCount($posts);
+	}
+
+	function feedCount($posts) {
 		echo "<pre>";
-		print_r($posts['paging']);
-		// feedCount($posts);
+		print_r($posts);
+
+		if(!empty($posts['paging']['next'])) {
+			$link = $posts['paging']['next'];
+			$link = str_replace("https://graph.facebook.com/v1.0/", "", $link);
+			$data = $fbObject->api($link);
+			if(!empty($data['data'])) {
+				$postCount += count($data['data']);
+				feedCount($data);
+			}
+		}
 	}
 exit;
-	// function feedCount($posts) {
-	// 	if(!empty($posts['paging']['next'])) {
-	// 		$link = $posts['paging']['next'];
-	// 		$link = str_replace("https://graph.facebook.com", "", $link);
-	// 		$data = $fbObject->api($link);
-	// 		if(!empty($data['data'])) {
-	// 			$postCount += count($data['data']);
-	// 			feedCount($data);
-	// 		}
-	// 	}
-	// }
-
 	$videos = $fbObject->api('/' . $page_id . '/videos?offset=0');
 	$videoCount = count($videos['data']);
 ?>
