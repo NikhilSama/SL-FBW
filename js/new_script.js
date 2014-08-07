90ff232291b3c80bc0ba9f7af60588de943b2262
Deepak Bansal <deepak.bansal@snaplion.com>
2014-08-07 12:30:46 +0530

    Image Center
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- js/new_script.js
+++ js/new_script.js
@@ -564,7 +564,8 @@ function crop(blob, obj) {
 								// dummyImage.onload = hideLoader();
 								//attch here a function to handle the checking if the image has been sent to snaplion server without any error
 								dummyImage.attr("src",new_InkBlob.url);
-								// dummyImage.css({"width":"216px","height":"383px","margin-top":"0px","margin-left":"0px","top":"0px","left":"0px"});
+								dummyImage.css({"top":"0","left":"0"});
+								// position: absolute;top: 50%;left: 50%;margin: -50px -60px;"
 								dummyImage.attr("data-value","1");
 							} else if(target_element == "iconImage" ) {
 								iconUpload = new Object();
