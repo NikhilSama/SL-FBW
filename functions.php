9457142466bbbd120ae9eea3b41269dc371696f8
Deepak Bansal <deepak.bansal@snaplion.com>
2014-08-08 12:21:43 +0530

    Minor
    Signed-off-by: Deepak Bansal <deepak.bansal@snaplion.com>

--- functions.php
+++ functions.php
@@ -1089,13 +1089,15 @@
 			$submitDataResult = array(
 				'mobapp_id' => $data['mobapp_id'],
 				'message' => "data has been submitted",
-				'status' => $result_data['result']['status']
+				'status' => $result_data['result']['status'],
+				'data' => $data['section']
 			);
 		} else {
 			$submitDataResult = array(
 				'mobapp_id' => $data['mobapp_id'],
 				'message' => "there was an error while submitting your data",
-				'status' => $result_data['result']['status']
+				'status' => $result_data['result']['status'],
+				'data' => $data['section']
 			);
 		}
 
